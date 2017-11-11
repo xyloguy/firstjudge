96c76bc6aa7b77b2df8242d2ded2643597588ff9
James Price <james@jamesmprice.com>
2017-11-11 05:02:01 -0700

    Added round seeder that creates static rounds 1-4

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(TeamSeeder::class);
         $this->call(SponsorSeeder::class);
+        $this->call(RoundSeeder::class);
     }
 }
