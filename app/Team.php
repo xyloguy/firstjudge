09a21043a951a4999b9f57ef1856214eaf08aaaf
James Price <james@jamesmprice.com>
2017-11-11 04:24:59 -0700

    Results page is working

--- app/Team.php
+++ app/Team.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Team extends Model
 {
@@ -12,8 +13,26 @@ class Team extends Model
         return $this->belongsTo('App\Tournament');
     }
 
-    public function scores()
+    public function scores(): HasMany
     {
         return $this->hasMany('App\Score');
+    }
+
+    public function scores_for_round($round_id)
+    {
+        $score = $this->scores()->where('round_id',$round_id)->first();
+        if($score){
+            return $score->round_total;
+        }
+        return 0;
+    }
+
+    public function highest_score()
+    {
+        $score = $this->scores()->max('round_total');
+        if($score){
+            return $score;
+        }
+        return 0;
     }
 }
